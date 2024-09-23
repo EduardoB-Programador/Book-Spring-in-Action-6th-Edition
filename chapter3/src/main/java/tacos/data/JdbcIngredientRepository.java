ce181d27a63fff8d1efbeecd3e84b8c95c182290
EduardoB-Programador <eduardobraga.contato@gmail.com>
2024-09-22 22:49:10 -0300

    continuing with chapter 3
    this commit's purpose is to state that, apparently, the H2 database isn't work, so the files "data.sql" and "schema.sql" are not working properly, if I keep them in the project the aplication doesn't work, this means I'll probably do data persistency in another way in the future, and not following the book exactly

--- chapter3/src/main/java/tacos/data/JdbcIngredientRepository.java
+++ chapter3/src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -4,6 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
 
+
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
