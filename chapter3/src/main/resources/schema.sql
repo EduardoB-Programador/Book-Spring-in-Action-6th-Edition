ec901202d6ccf1b5392b1efb96ecedf1876be795
EduardoB-Programador <eduardobraga.contato@gmail.com>
2024-09-27 17:32:56 -0300

    I screwed up
    damn this all, I'll complete this in another way, everything is getting on my nerves, nothing works and I don't even know why, I'll probably restart this book, use spring tools suite as the main IDE since Intellij IDEA CE don't want to collaborate

--- chapter3/src/main/resources/schema.sql
+++ chapter3/src/main/resources/schema.sql
@@ -1,35 +1,35 @@
-create table if not exists Taco_Order (
-    id identity,
-    delivery_Name varchar(50) not null,
-    delivery_Street varchar(50) not null,
-    delivery_City varchar(50) not null,
-    delivery_State varchar(2) not null,
-    delivery_Zip varchar(10) not null,
-    cc_number varchar(16) not null,
-    cc_expiration varchar(5) not null,
-    cc_cvv varchar(3) not null,
-    placed_at timestamp not null
-);
-
-create table if not exists Taco (
-    id identity,
-    name varchar(50) not null,
-    taco_order bigint not null,
-    taco_order_key bigint not null,
-    created_at timestamp not null
-);
-
-create table if not exists Ingredient_Ref (
-    ingredient varchar(4) not null,
-    taco bigint not null,
-    taco_key bigint not null
-);
-
-create table if not exists Ingredient (
-    id varchar(4) not null,
-    name varchar(25) not null,
-    type varchar(10) not null
-);
-
-alter table Taco add foreign key (taco_order) references Taco_Order(id);
-alter table Ingredient_Ref add foreign key (ingredient) references Ingredient(id);
+--CREATE TABLE if NOT EXISTS Taco_Order (
+--    id IDENTITY,
+--    delivery_name VARCHAR(50) NOT NULL,
+--    delivery_street VARCHAR(50) NOT NULL,
+--    delivery_city VARCHAR(50) NOT NULL,
+--    delivery_state VARCHAR(2) NOT NULL,
+--    delivery_zip VARCHAR(10) NOT NULL,
+--    cc_number VARCHAR(16) NOT NULL,
+--    cc_expiration VARCHAR(5) NOT NULL,
+--    cc_cvv VARCHAR(3) NOT NULL,
+--    placed_at TIMESTAMP NOT NULL
+--);
+--
+--CREATE TABLE if NOT EXISTS Taco (
+--    id IDENTITY,
+--    name VARCHAR(50) NOT NULL,
+--    taco_order BIGINT NOT NULL,
+--    taco_order_key BIGINT NOT NULL,
+--    created_at TIMESTAMP NOT NULL
+--);
+--
+--CREATE TABLE if NOT EXISTS Ingredient_Ref (
+--    ingredient VARCHAR(4) NOT NULL,
+--    taco  BIGINT NOT NULL,
+--    taco_key BIGINT NOT NULL
+--);
+--
+--CREATE TABLE if NOT EXISTS Ingredient (
+--    id VARCHAR(4) NOT NULL,
+--    name VARCHAR(25) NOT NULL,
+--    type VARCHAR(10) NOT NULL
+--);
+--
+--ALTER TABLE Taco ADD FOREIGN KEY (taco_order) REFERENCES Taco_Order(id);
+--ALTER TABLE Ingredient_Ref ADD FOREIGN KEY (ingredient) REFERENCES Ingredient(id);
